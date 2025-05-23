9c987e0ad75c350c5c96466139660a6eb7cf4dbe
Chris Athanas <realityexpander@gmail.com>
2025-05-22 19:42:57 -0500

    improve AgendaRoutes: enhance sync logic to check event host before deletion

--- src/main/kotlin/com/realityexpander/routing/AgendaRoutes.kt
+++ src/main/kotlin/com/realityexpander/routing/AgendaRoutes.kt
@@ -21,95 +21,109 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 
 fun Route.agenda() {
-    val eventDataSource: EventDataSource by inject()
-    val taskDataSource: TaskDataSource by inject()
-    val reminderDataSource: ReminderDataSource by inject()
-    val agendaDataSource: AgendaDataSource by inject()
-    val attendeeDataSource: AttendeeDataSource by inject()
-    val s3: AmazonS3 by inject()
-    val dispatchers by inject<DispatcherProvider>()
+	val eventDataSource: EventDataSource by inject()
+	val taskDataSource: TaskDataSource by inject()
+	val reminderDataSource: ReminderDataSource by inject()
+	val agendaDataSource: AgendaDataSource by inject()
+	val attendeeDataSource: AttendeeDataSource by inject()
+	val s3: AmazonS3 by inject()
+	val dispatchers by inject<DispatcherProvider>()
 
-    authenticate("jwt") {
-        // Gets the agenda for the given date
-        get("agenda") {
-            val time = call.parameters["time"]?.toLongOrNull()?.let {
-                try {
-                    ZonedDateTime.ofInstant(Instant.ofEpochMilli(it), ZoneId.of("UTC"))
-                } catch (e: Exception) {
-                    call.respond(
-                        HttpStatusCode.BadRequest,
-                        ErrorMessage("The time in an invalid format")
-                    )
-                    return@get
-                }
-            } ?: kotlin.run {
-                try {
-                    LocalDateTime.now().atZone(ZoneId.of("UTC"))
-                } catch (e: Exception) {
-                    call.respond(
-                        HttpStatusCode.BadRequest,
-                        ErrorMessage("The time is in an invalid format")
-                    )
-                    return@get
-                }
-            }
+	authenticate("jwt") {
+		// Gets the agenda for the given date
+		get("agenda") {
+			val time = call.parameters["time"]?.toLongOrNull()?.let {
+				try {
+					ZonedDateTime.ofInstant(Instant.ofEpochMilli(it), ZoneId.of("UTC"))
+				} catch (e: Exception) {
+					call.respond(
+						HttpStatusCode.BadRequest,
+						ErrorMessage("The time in an invalid format")
+					)
+					return@get
+				}
+			} ?: kotlin.run {
+				try {
+					LocalDateTime.now().atZone(ZoneId.of("UTC"))
+				} catch (e: Exception) {
+					call.respond(
+						HttpStatusCode.BadRequest,
+						ErrorMessage("The time is in an invalid format")
+					)
+					return@get
+				}
+			}
 
-            val agenda = agendaDataSource.getAgenda(call.userId, time.toLocalDate())
-            val agendaDto = agenda.toAgendaDto(
-                callerUserId = call.userId,
-                attendeeDataSource = attendeeDataSource,
-                s3 = s3,
-                ioDispatcher = dispatchers.io
-            )
+			val agenda = agendaDataSource.getAgenda(call.userId, time.toLocalDate())
+			val agendaDto = agenda.toAgendaDto(
+				callerUserId = call.userId,
+				attendeeDataSource = attendeeDataSource,
+				s3 = s3,
+				ioDispatcher = dispatchers.io
+			)
 
-            call.respond(HttpStatusCode.OK, agendaDto)
-        }
-    }
-    authenticate("jwt") {
-         // Syncs the agenda with the server by deleting the events, tasks, and reminders
-        post("syncAgenda") {
-            val request = call.receiveNullable<SyncAgendaRequest>() ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, ErrorMessage("Missing or invalid body"))
-                return@post
-            }
+			call.respond(HttpStatusCode.OK, agendaDto)
+		}
+	}
+	authenticate("jwt") {
+		// Syncs the agenda with the server by deleting the events, tasks, and reminders
+		post("syncAgenda") {
+			val request = call.receiveNullable<SyncAgendaRequest>() ?: kotlin.run {
+				call.respond(HttpStatusCode.BadRequest, ErrorMessage("Missing or invalid body"))
+				return@post
+			}
 
-            val eventJob = coroutineScope {
-                launch {
-                    request.deletedEventIds.forEach { id -> eventDataSource.deleteEvent(id) }
-                }
-            }
-            val taskJob = coroutineScope {
-                launch {
-                    request.deletedTaskIds.forEach { id -> taskDataSource.deleteTask(id, call.userId) }
-                }
-            }
-            val reminderJob = coroutineScope {
-                launch {
-                    request.deletedReminderIds.forEach { id -> reminderDataSource.deleteReminder(id, call.userId) }
-                }
-            }
+			val eventJob = coroutineScope {
+				launch {
+					request.deletedEventIds.forEach { id ->
+						eventDataSource.deleteEvent(id)
+					}
 
-            eventJob.join()
-            taskJob.join()
-            reminderJob.join()
+					// Check if the user is the host of the event before deleting
+					request.deletedEventIds.forEach { id ->
+						if (eventDataSource.isHost(call.userId, id)) {
+							eventDataSource.deleteEvent(id)
+						} else {
+							call.respond(HttpStatusCode.Forbidden, ErrorMessage("You are not the host of this event"))
+							return@launch
+						}
+					}
+				}
+			}
+			val taskJob = coroutineScope {
+				launch {
+					request.deletedTaskIds.forEach { id ->
+						taskDataSource.deleteTask(id, call.userId)
+					}
+				}
+			}
+			val reminderJob = coroutineScope {
+				launch {
+					request.deletedReminderIds.forEach { id -> reminderDataSource.deleteReminder(id, call.userId) }
+				}
+			}
 
-            call.respond(HttpStatusCode.OK)
-        }
-    }
+			eventJob.join()
+			taskJob.join()
+			reminderJob.join()
 
-    authenticate("jwt") {
-         // Gets the full agenda for the given date
-        get("fullAgenda") {
-            val agenda = agendaDataSource.getFullAgenda(call.userId)
-            call.respond(
-                HttpStatusCode.OK,
-                agenda.toAgendaDto(
-                    callerUserId = call.userId,
-                    attendeeDataSource = attendeeDataSource,
-                    s3 = s3,
-                    ioDispatcher = dispatchers.io
-                )
-            )
-        }
-    }
+			call.respond(HttpStatusCode.OK)
+		}
+	}
+
+	authenticate("jwt") {
+		// Gets the full agenda for the given date
+		get("fullAgenda") {
+			val agenda = agendaDataSource.getFullAgenda(call.userId)
+			call.respond(
+				HttpStatusCode.OK,
+				agenda.toAgendaDto(
+					callerUserId = call.userId,
+					attendeeDataSource = attendeeDataSource,
+					s3 = s3,
+					ioDispatcher = dispatchers.io
+				)
+			)
+		}
+	}
 }
